af85a7babf4cac96670b2fd23efd3c7cf571035c
areyouokevan <discordleaf@protonmail.com>
2020-12-23 23:21:10 +0100

    80

--- main.py
+++ main.py
@@ -15,5 +15,10 @@ def interactions():
     print(request.json)
 
 
+@app.route('/')
+def root():
+    return jsonify({message: "gtfo"})
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=80)
