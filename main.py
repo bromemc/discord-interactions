07c0c7f3cb380c98aa0a72d6ded139750fd39126
areyouokevan <discordleaf@protonmail.com>
2020-12-23 23:09:56 +0100

    thongs

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask, jsonify
+
+from discord_interactions import verify_key_decorator, InteractionType, InteractionResponseType
+
+CLIENT_PUBLIC_KEY = "cc8d90dfdb6b3efa4fbd586a23682c69e1463f856313e3e2f62ddd55cb72402a"
+
+app = Flask(__name__)
+
+
+@app.route('/interactions', methods=['POST'])
+@verify_key_decorator(CLIENT_PUBLIC_KEY)
+def interactions():
+    print(request.json)
